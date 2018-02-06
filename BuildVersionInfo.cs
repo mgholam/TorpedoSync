99326569ebcb49982b08e318fa789987c32ac471
M. Gholam <mgholam@gmail.com>
2018-02-06 15:48:18 +0330

    1.1.0 - code refactoring - working with mono on pi and ubuntu

--- BuildVersionInfo.cs
+++ BuildVersionInfo.cs
@@ -1,6 +1,6 @@
 //using System.Reflection;
-// build number = 492
+// build number = 534
 // build version = 1.1.0
 
-[assembly: System.Reflection.AssemblyFileVersion("1.1.0.492")]
+[assembly: System.Reflection.AssemblyFileVersion("1.1.0.534")]
 [assembly: System.Reflection.AssemblyVersion("1.0.0.0")] 
