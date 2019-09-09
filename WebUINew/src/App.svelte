508e77e7a4ce69a0219991b656d9f07a6cb21894
M. Gholam <mgholam@gmail.com>
2019-09-09 20:53:19 +0430

    - refactored tabs out of app into own component

--- WebUINew/src/App.svelte
+++ WebUINew/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { tick, onMount } from "svelte";
+  import Tabs from "./UI/tabs.svelte";
   // import NavPanel from "./UI/nav.svelte";
   import Button from "./UI/Button.svelte";
   import Fa from "svelte-fa";
@@ -11,6 +12,7 @@
   import Logs from "./pages/logs.svelte";
   import Settings from "./pages/settings.svelte";
 
+  let mainid = "mainid";
   let activetabid = "";
   let tabs = [
     // {
@@ -24,18 +26,33 @@
   let title = "";
   let modalyes = false;
   let server = "";
+  // let TABS;
+
+  // $: console.log(TABS);
 
   onMount(() => {
-    addtab({ name: "share", description: "Shares", icon: icons.faFolderOpen });
+    addtab({
+      name: "share",
+      description: "Shares",
+      icon: icons.faFolderOpen
+    });
     addtab({
       name: "connection",
       description: "Connections",
       icon: icons.faExchangeAlt
     });
-    addtab({ name: "settings", description: "Settings", icon: icons.faCog });
+    addtab({
+      name: "settings",
+      description: "Settings",
+      icon: icons.faCog
+    });
     addtab({ name: "logs", description: "Logs", icon: icons.faList });
-    addtab({ name: "help", description: "Help", icon: icons.faQuestionCircle });
-    tick().then(() => changetab(tabs[0].id));
+    addtab({
+      name: "help",
+      description: "Help",
+      icon: icons.faQuestionCircle
+    });
+    tick().then(() => (activetabid = tabs[0].id));
     setTimeout(() => {
       // kludge to get the server
       server = window.ServerURL;
@@ -71,22 +88,13 @@
           target: document.getElementById(id)
         });
         break;
-      // case "docview":
-      //   tab = new DocView({
-      //     target: document.getElementById(id),
-      //     props: { docid: args ? args : null }
-      //   });
-      //   tab.$on("showrevs", event => {
-      //     addtab({ name: "dochistory", description: "History" }, event.detail);
-      //   });
-      //   break;
     }
 
     return tab;
   }
 
   function addtab(nav, args) {
-    var e = document.getElementById("mainid");
+    var e = document.getElementById(mainid);
     // create div
     var divtest = document.createElement("div");
     var id = "id" + Math.floor(Math.random() * 1000);
@@ -94,7 +102,6 @@
     divtest.className = "container active";
     e.appendChild(divtest);
 
-    activetabid = id;
     tick().then(() => {
       // create svelte component and bind to dom
       var tab = createcomponent(nav.name, id, args);
@@ -109,64 +116,8 @@
           icon: nav.icon
         }
       ];
-      changetab(id);
+      activetabid = id;
     });
-  }
-
-  function removetab(id) {
-    // console.log(tabs);
-    var t = tabs.find(x => x.id === id);
-    if (t !== null && t.obj !== null) {
-      // remove svelte component
-      t.obj.$destroy();
-      // remove from list
-      tabs = tabs.filter(x => x.id !== id);
-      // remove div from dom
-      var e = document.querySelector("#mainid #" + id);
-      e.remove();
-    }
-  }
-
-  function showtab(nav) {
-    // create content component
-    // create new tab
-    // bind content to tab
-    addtab(nav);
-  }
-
-  function changetab(id) {
-    activetabid = id;
-    tabs.forEach(x => {
-      x.active = x.id == id ? true : false;
-      x.obj.active = x.active;
-    });
-    tabs = tabs;
-  }
-
-  function closetab(id) {
-    var i = tabs.findIndex(x => x.id === id);
-    i--;
-    changetab(tabs[i].id);
-    removetab(id);
-  }
-
-  function closealltabs() {
-    showmsg = false;
-
-    tabs.forEach(x => {
-      if (x.title !== "Help") closetab(x.id);
-    });
-  }
-
-  function closeall() {
-    showmsg = true;
-    modalyes = false;
-
-    if (tabs.length == 1) title = "No Tabs to close";
-    else {
-      title = "Do you want to close all the tabs?";
-      modalyes = true;
-    }
   }
 </script>
 
@@ -207,18 +158,6 @@
     min-height: 90vh;
   }
 
-  /* :global(.gi) {
-    position: relative;
-    top: 1px;
-    display: inline-block;
-    font-family: "Glyphicons Halflings";
-    font-style: normal;
-    font-weight: normal;
-    line-height: 1;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  } */
-
   .Container {
     width: 100%;
     backface-visibility: hidden;
@@ -240,86 +179,6 @@
     display: none;
   }
 
-  /* .Left {
-    width: 220px;
-    float: left;
-  } */
-
-  .tab-links:after {
-    display: block;
-    clear: both;
-    content: "";
-  }
-
-  .tab-links li {
-    margin: 0px 1px;
-    float: left;
-    list-style: none;
-  }
-
-  .tab-links {
-    -webkit-padding-start: 5px;
-    border-radius: 5px 5px 0px 0px;
-    margin-bottom: 0px;
-    margin-top: 10px;
-  }
-
-  .tab-links div {
-    padding: 9px 15px;
-    display: inline-block;
-    border-radius: 5px 5px 0px 0px;
-    background: var(--theme-darker);
-    font-size: 16px;
-    font-weight: 600;
-    color: var(--theme-color);
-    transition: all linear 0.15s;
-    cursor: pointer;
-  }
-
-  /* .tab-links div #close {
-    margin-left: 10px;
-    padding: 2px;
-  }
-
-  .tab-links div #close:hover {
-    background: red;
-    color: white;
-    text-align: center;
-    -webkit-border-radius: 15px;
-    -moz-border-radius: 15px;
-    border-radius: 15px;
-    -moz-box-shadow: 1px 1px 3px #000;
-    -webkit-box-shadow: 1px 1px 3px #000;
-    box-shadow: 1px 1px 3px #000;
-  } */
-
-  .tab-links div:hover {
-    background: var(--theme-light);
-    color: var(--theme-darker);
-    text-decoration: none;
-  }
-
-  li.active div,
-  li.active {
-    background: var(--theme-white) !important;
-    border-radius: 5px 5px 0px 0px;
-    border: 1px;
-    color: var(--theme-darker);
-  }
-  /* .closeall {
-    margin-left: -35px;
-    color: white;
-    padding: 8px;
-    float: right !important;
-    font-weight: bolder;
-    text-align: center;
-  }
-  .closeall:hover {
-    background: red;
-    -webkit-border-radius: 15px;
-    -moz-border-radius: 15px;
-    border-radius: 15px;
-  } */
   .brand {
     color: var(--theme-white);
     font-size: x-large;
@@ -337,28 +196,10 @@
   </div> -->
 
   <div class="Middle">
-    <ul class="tab-links">
-      <!-- <span class="closeall" on:click={closeall} title="Close all tabs">X</span> -->
-      {#each tabs as tab (tab.id)}
-        <li
-          id={tab.id}
-          class:active={tab.id === activetabid}
-          on:click={() => changetab(tab.id)}>
-          <div>
-            <label>
-              <Fa icon={tab.icon} />
-              <!-- <span class={tab.icon} /> -->
-              {tab.title}
-            </label>
-            <!-- {#if tab.title !== 'Help'}
-              <label id="close" on:click={() => closetab(tab.id)}>X</label>
-            {/if} -->
-          </div>
-        </li>
-      {/each}
-    </ul>
 
-    <div id="mainid" />
+    <Tabs {tabs} {activetabid} {mainid}/>
+
+    <div id={mainid} />
 
   </div>
 </div>
---
+++ WebUINew/src/UI/tabs.svelte
@@ -0,0 +1,146 @@
+<script>
+  import Fa from "svelte-fa";
+  import icons from "../icons.js";
+
+  export let mainid;
+  export let tabs;
+  export let activetabid = "";
+
+  $: changetab(activetabid);
+
+  function removetab(id) {
+    // console.log(tabs);
+    var t = tabs.find(x => x.id === id);
+    if (t !== null && t.obj !== null) {
+      // remove svelte component
+      t.obj.$destroy();
+      // remove from list
+      tabs = tabs.filter(x => x.id !== id);
+      // remove div from dom
+      var e = document.querySelector("#" + mainid + " #" + id);
+      e.remove();
+    }
+  }
+
+  // function showtab(nav) {
+  //   // create content component
+  //   // create new tab
+  //   // bind content to tab
+  //   addtab(nav);
+  // }
+
+  function changetab(id) {
+    activetabid = id;
+    tabs.forEach(x => {
+      x.active = x.id == id ? true : false;
+      x.obj.active = x.active;
+    });
+    tabs = tabs;
+  }
+
+  function closetab(id) {
+    var i = tabs.findIndex(x => x.id === id);
+    i--;
+    changetab(tabs[i].id);
+    removetab(id);
+  }
+
+  function closealltabs() {
+    showmsg = false;
+
+    tabs.forEach(x => {
+      if (x.title !== "Help") closetab(x.id);
+    });
+  }
+
+  function closeall() {
+    showmsg = true;
+    modalyes = false;
+
+    if (tabs.length == 1) title = "No Tabs to close";
+    else {
+      title = "Do you want to close all the tabs?";
+      modalyes = true;
+    }
+  }
+</script>
+
+<style>
+  .tab-links:after {
+    display: block;
+    clear: both;
+    content: "";
+  }
+
+  .tab-links li {
+    margin: 0px 1px;
+    float: left;
+    list-style: none;
+  }
+
+  .tab-links {
+    -webkit-padding-start: 5px;
+    border-radius: 5px 5px 0px 0px;
+    margin-bottom: 0px;
+    margin-top: 10px;
+  }
+
+  .tab-links div {
+    padding: 9px 15px;
+    display: inline-block;
+    border-radius: 5px 5px 0px 0px;
+    background: var(--theme-darker);
+    font-size: 16px;
+    font-weight: 600;
+    color: var(--theme-color);
+    transition: all linear 0.15s;
+    cursor: pointer;
+  }
+
+  .tab-links div:hover {
+    background: var(--theme-light);
+    color: var(--theme-darker);
+    text-decoration: none;
+  }
+
+  li.active div,
+  li.active {
+    background: var(--theme-white) !important;
+    border-radius: 5px 5px 0px 0px;
+    border: 1px;
+    color: var(--theme-darker);
+  }
+  /* .closeall {
+    margin-left: -35px;
+    color: white;
+    padding: 8px;
+    float: right !important;
+    font-weight: bolder;
+    text-align: center;
+  }
+  .closeall:hover {
+    background: red;
+    -webkit-border-radius: 15px;
+    -moz-border-radius: 15px;
+    border-radius: 15px;
+  } */
+</style>
+
+<svelte:options accessors={true} />
+
+<ul class="tab-links">
+  <!-- <span class="closeall" on:click={closeall} title="Close all tabs">X</span> -->
+  {#each tabs as tab (tab.id)}
+    <li
+      id={tab.id}
+      class:active={tab.id === activetabid}
+      on:click={() => changetab(tab.id)}>
+      <div>
+        <label>
+          <Fa icon={tab.icon} />
+          {tab.title}
+        </label>
+      </div>
+    </li>
+  {/each}
+</ul>
